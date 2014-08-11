365e20b01709efa61863354d1be793db737f4441
Shubham <shubham.kedia2010@gmail.com>
2014-08-12 02:15:03 +0530

    Nil value bugs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,11 +8,15 @@ class JobsController < ApplicationController
   def search
     @device= URI.encode(request.user_agent)
     @ip=request.remote_ip
-    @query=params[:q]
-    @loc=URI.encode(params[:l])
-    @url="http://api.indeed.com/ads/apisearch?publisher=3083679641906722&q=#{@query}&l=#{@loc}&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=#{@ip}&useragent=#{@device}&v=2&format=json"
-    @results=open(@url).read
-    @jobs = JSON.parse(@results)
-    @jobs = @jobs["results"]
+    @query=params[:q]||=""
+    @loc=URI.encode(params[:l]||="")
+    @jobs=[]
+    if @query.present? || @loc.present?
+      @url="http://api.indeed.com/ads/apisearch?publisher=3083679641906722&q=#{@query}&l=#{@loc}&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=#{@ip}&useragent=#{@device}&v=2&format=json"
+      @results=open(@url).read
+      @jobs = JSON.parse(@results)
+      @jobs = @jobs["results"]
+    end
+    # binding.pry
   end
 end
